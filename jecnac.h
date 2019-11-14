65c3d71cb6acc94cd3b79096ffdc2848b75646da
David Šorm <david.sorm@gmail.com>
2019-11-14 16:50:57 +0100

    Initial commit

---
+++ jecnac.h
@@ -0,0 +1,22 @@
+//
+// Created by david on 19.09.19.
+//
+
+#ifndef PRUMERC_JECNAC_H
+#define PRUMERC_JECNAC_H
+#endif //PRUMERC_JECNAC_H
+
+#define inteligente ;
+#define budte_zticha return
+#define budte_zticha_vy return
+#define polointeligente 0;
+#define nemluvte_do_toho break
+#define intidzr int
+#define limovy_intidzr long
+#define limovy_flout double
+#define zvaz_limovou sizeof
+#define kdyz_budai_souhlasi_ze if
+#define napis_na_tabuli printf
+#define slizni_z_tabule scanf
+#define starek for
+#define ulozte_si_main_c main
---
+++ main.c
@@ -0,0 +1,20 @@
+//
+// Created by david on 05.11.19.
+//
+
+#include <stdio.h>
+
+int* vratSrackySlabyho() {
+    int srackySlabyho[10];
+    srackySlabyho[0] = 1;
+    srackySlabyho[1] = 2;
+    srackySlabyho[2] = 4;
+    srackySlabyho[3] = 8;
+    srackySlabyho[4] = 16;
+    return &srackySlabyho;
+}
+
+void main() {
+    int* srackySlabyho = vratSrackySlabyho();
+    printf("sracka slabyho c. 4 je %d", &srackySlabyho[4]);
+}
